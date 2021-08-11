3515a82f8f12a03f2e4d402453b15cfc33114916
Artem Kuznetsov <at0m616@yandex.ru>
2021-08-11 11:29:04 +0500

    Initial commit

---
+++ todolist/src/main/java/com/crud/demo311/Demo311Application.java
@@ -0,0 +1,13 @@
+package com.crud.demo311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo311Application.class, args);
+	}
+
+}
---
+++ todolist/src/test/java/com/crud/demo311/Demo311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.demo311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
